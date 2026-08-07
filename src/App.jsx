52a737d80dee29174af5468b2c3d0f52d4a784c9
Goodday1989 <2012alexlst@gmail.com>
2026-08-07 16:36:03 +0800

    adding states and handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Gameboard from "./components/Gameboard";
 import GameOverModal from "./components/GameOverModal";
 import Scoreboard from "./components/Scoreboard";
 
 export default function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [clickedId, setClickedId] = useState([]);
+  const [isGameOver, setIsGameOver] = useState(false);
   return (
     <main>
       <h1>Alex's Memory Card Game</h1>
-      <Scoreboard />
+      <Scoreboard currentScore={currentScore} highScore={highScore} />
       <Gameboard />
-      <GameOverModal />
+      <GameOverModal isGameOver={isGameOver} />
     </main>
   );
 }
