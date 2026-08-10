765127973433c79e5aee59bec72b8aa7f6773777
Goodday1989 <2012alexlst@gmail.com>
2026-08-10 10:52:39 +0800

    scoreboard.jsx updated props

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,10 +1,10 @@
-export default function Scoreboard() {
+export default function Scoreboard({ currentScore, highScore, round }) {
   return (
     <section className="scoreboard">
       <h2>Scoreboard</h2>
-      <p>Round :</p>
-      <p>Your current score :</p>
-      <p>Your high score :</p>
+      <p>Round : {round}</p>
+      <p>Your current score : {currentScore}</p>
+      <p>Your high score : {highScore}</p>
     </section>
   );
 }
